f7ed105aed1015044f89e7e96a4a9051dd324ea3
Stella Obua <stellaobua123@gmail.com>
2023-03-30 06:36:30 -0700

    Changed the code

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,30 @@
 # Python101 Final Project
 import random
+# While loop
 while True:
-    correct_answer = random.choice(["rock", "paper", "scissors"])
-    user_guess = input("Rock, Paper, Scissors? ").lower()
-    if user_guess == correct_answer:
-        print("Yay you won!")
+    # Get input from the user
+    user_input = input("Choose: rock, paper, scissors: ")
+    user_input = user_input.lower()
+    print(user_input)
+    # If the user wants to quit the game.
+    if user_input == "quit":
+        print("You have quit the game.")
+        break
+    # If the user does not input either rock, paper or scissors
+    if user_input != 'rock' and user_input != 'paper' and user_input != 'scissors':
+        print("Please choose the right option.")
+        continue
+    # The computer's choice
+    computer_answer = random.choice(["rock", "paper", "scissors"])
+    print(f"The computer chose: {computer_answer}")
+    if user_input == "rock" and computer_answer == "scissors":
+        print("You win!")
+        break
+    elif user_input == "paper" and computer_answer == "rock":
+        print("You win!")
+        break
+    elif user_input == "scissors" and computer_answer == "paper":
+        print("You win!")
         break
     else:
-        print("Wrong try again!")
-        print(f"The Correct answer was: {correct_answer.upper()}")
+        print("You lost. Try again")
